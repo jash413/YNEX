306f5f28367b7700d18ba6bdad979f5e6db29fd5
Majithia Devansh <123489552+mdevansh3012@users.noreply.github.com>
2024-04-23 16:51:03 +0530

    FEAT : changes

--- shared/layout-components/forms/create-update-bid.js
+++ shared/layout-components/forms/create-update-bid.js
@@ -7,10 +7,8 @@ import { z } from "zod";
 const today = new Date();
 const isoDate = today.toISOString();
 import network from "@/config";
-import BidDetails from "@/shared/layout-components/forms/bid-details";
 
 const formDataSchema = z.object({
-  budget_amount: z.string().nonempty({ message: "Budget Amount is required" }),
   bid_amount_from_sub: z
     .string()
     .nonempty({ message: "Bid Amount From Sub is required" }),
@@ -38,11 +36,10 @@ const CreateUpdateBid = (props) => {
     selectedTask: "",
     cost_code_id: "",
     description: "",
-    budget_amount: "",
     bid_amount_from_sub: "",
     bid_details_from_sub: "",
-    bid_inscope: "",
-    bid_outscope: "",
+    bid_inscope: [{ bidDetail: "" }],
+    bid_outscope: [{ bidDetail: "" }],
     bid_payment_terms: "",
     bid_recieved_date: isoDate,
     bid_status: "",
@@ -62,11 +59,10 @@ const CreateUpdateBid = (props) => {
             selectedTask: response.data.selectedTask,
             cost_code_id: response.data.cost_code_id,
             description: response.data.description,
-            budget_amount: response.data.budget_amount,
             bid_amount_from_sub: response.data.bid_amount_from_sub,
             bid_details_from_sub: response.data.bid_details_from_sub,
-            bid_inscope: response.data.bid_inscope,
-            bid_outscope: response.data.bid_outscope,
+            // bid_inscope: response.data.bid_inscope,
+            // bid_outscope: response.data.bid_outscope,
             bid_payment_terms: response.data.bid_payment_terms,
             bid_recieved_date: response.data.bid_recieved_date,
             bid_status: response.data.bid_status,
@@ -90,6 +86,25 @@ const CreateUpdateBid = (props) => {
     setSelectedProject(selectedProject);
   };
 
+  const handleBidChange = (e, index, field) => {
+    const newBids = [...formData[field]];
+    newBids[index][e.target.name] = e.target.value;
+    setFormData({ ...formData, [field]: newBids });
+  };
+
+  const handleAddBid = (field) => {
+    setFormData({
+      ...formData,
+      [field]: [...formData[field], { bidDetail: "" }],
+    });
+  };
+
+  const handleRemoveBid = (index, field) => {
+    const newBids = [...formData[field]];
+    newBids.splice(index, 1);
+    setFormData({ ...formData, [field]: newBids });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -101,7 +116,6 @@ const CreateUpdateBid = (props) => {
         selectedTask: "",
         cost_code_id: "",
         description: "",
-        budget_amount: "",
         bid_amount_from_sub: "",
         bid_details_from_sub: "",
         bid_inscope: "",
@@ -118,65 +132,6 @@ const CreateUpdateBid = (props) => {
   };
   return (
     <div>
-      <div id="hs-full-screen-modal" className="hs-overlay hidden ti-modal">
-        <div className="hs-overlay-open:mt-0 ti-modal-box mt-10 !m-0 !max-w-full !w-full">
-          <div className="ti-modal-content !rounded-none">
-            <div className="ti-modal-header">
-              <h6 className="ti-modal-title">PREVIEW BID DETAILS</h6>
-              <button
-                type="button"
-                className="hs-dropdown-toggle ti-modal-close-btn"
-                data-hs-overlay="#hs-full-screen-modal"
-              >
-                <span className="sr-only">Close</span>
-                <svg
-                  className="w-3.5 h-3.5"
-                  width="8"
-                  height="8"
-                  viewBox="0 0 8 8"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M0.258206 1.00652C0.351976 0.912791 0.479126 0.860131 0.611706 0.860131C0.744296 0.860131 0.871447 0.912791 0.965207 1.00652L3.61171 3.65302L6.25822 1.00652C6.30432 0.958771 6.35952 0.920671 6.42052 0.894471C6.48152 0.868271 6.54712 0.854471 6.61352 0.853901C6.67992 0.853321 6.74572 0.865971 6.80722 0.891111C6.86862 0.916251 6.92442 0.953381 6.97142 1.00032C7.01832 1.04727 7.05552 1.1031 7.08062 1.16454C7.10572 1.22599 7.11842 1.29183 7.11782 1.35822C7.11722 1.42461 7.10342 1.49022 7.07722 1.55122C7.05102 1.61222 7.01292 1.6674 6.96522 1.71352L4.31871 4.36002L6.96522 7.00648C7.05632 7.10078 7.10672 7.22708 7.10552 7.35818C7.10442 7.48928 7.05182 7.61468 6.95912 7.70738C6.86642 7.80018 6.74102 7.85268 6.60992 7.85388C6.47882 7.85498 6.35252 7.80458 6.25822 7.71348L3.61171 5.06702L0.965207 7.71348C0.870907 7.80458 0.744606 7.85498 0.613506 7.85388C0.482406 7.85268 0.357007 7.80018 0.264297 7.70738C0.171597 7.61468 0.119017 7.48928 0.117877 7.35818C0.116737 7.22708 0.167126 7.10078 0.258206 7.00648L2.90471 4.36002L0.258206 1.71352C0.164476 1.61976 0.111816 1.4926 0.111816 1.36002C0.111816 1.22744 0.164476 1.10028 0.258206 1.00652Z"
-                    fill="currentColor"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="ti-modal-body">
-              <BidDetails
-                bidsData={{
-                  bid_amount_from_sub: formData.bid_amount_from_sub,
-                  bid_details_from_sub: formData.bid_details_from_sub,
-                  bid_inscope: formData.bid_inscope,
-                  bid_notes: formData.bid_notes,
-                  bid_outscope: formData.bid_outscope,
-                  bid_payment_terms: formData.bid_payment_terms,
-                  bid_recieved_date: formData.bid_recieved_date,
-                  bid_status: formData.bid_status,
-                  budget_amount: formData.budget_amount,
-                  cost_code_id: formData.cost_code_id,
-                  description: formData.description,
-                  selectedProject: formData.selectedProject,
-                  subcontractor_id: formData.subcontractor_id,
-                  selectedTask: formData.selectedTask,
-                }}
-                showEditBtn={false}
-              />
-            </div>
-            <div className="ti-modal-footer">
-              <button
-                type="button"
-                className="hs-dropdown-toggle ti-btn ti-btn-secondary-full"
-                data-hs-overlay="#hs-full-screen-modal"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
       <Seo title={`${formType === "update" ? "Update Bid" : "Create Bid"}`} />
       <Pageheader
         currentpage={`${formType === "update" ? "Update Bid" : "Create Bid"}`}
@@ -258,24 +213,11 @@ const CreateUpdateBid = (props) => {
           <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div className="mb-4">
-                <label htmlFor="budget_amount" className="form-label">
-                  Budget Amount
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="budget_amount"
-                  value={formData.budget_amount}
-                  onChange={handleInputChange}
-                />
-              </div>
-
-              <div className="mb-4">
                 <label htmlFor="bid_amount_from_sub" className="form-label">
                   Bid Amount From Sub
                 </label>
                 <input
-                  type="text"
+                  type="number"
                   className="form-control"
                   id="bid_amount_from_sub"
                   value={formData.bid_amount_from_sub}
@@ -324,35 +266,6 @@ const CreateUpdateBid = (props) => {
               </div>
 
               <div className="mb-4">
-                <label htmlFor="bid_outscope" className="form-label">
-                  Bid Outscope
-                </label>
-                <div className="input-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="bid_outscope"
-                    value={formData.bid_outscope}
-                    onChange={handleInputChange}
-                  />
-                </div>
-              </div>
-              <div className="mb-4">
-                <label htmlFor="bid_inscope" className="form-label">
-                  Bid Inscope
-                </label>
-                <div className="input-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="bid_inscope"
-                    value={formData.bid_inscope}
-                    onChange={handleInputChange}
-                  />
-                </div>
-              </div>
-
-              <div className="mb-4">
                 <label htmlFor="subcontractor_id" className="form-label">
                   Subcontractor ID
                 </label>
@@ -378,6 +291,85 @@ const CreateUpdateBid = (props) => {
                 />
               </div>
 
+              {formData.bid_inscope.map((bid, index) => (
+                <div className="row g-3 align-items-center" key={index}>
+                  <div className="col-md-5">
+                    <label
+                      htmlFor={`bid_inscope_${index}`}
+                      className="form-label"
+                    >
+                      Bid Inscope {index + 1}
+                    </label>
+                    <input
+                      type="text"
+                      id={`bid_inscope_${index}`}
+                      name="bidDetail"
+                      value={bid.bidDetail}
+                      onChange={(e) => handleBidChange(e, index, "bid_inscope")}
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div className="col-md-2">
+                    {index === formData.bid_inscope.length - 1 ? (
+                      <button
+                        className="ti-btn ti-btn-success-full mt-4"
+                        onClick={() => handleAddBid("bid_inscope")}
+                      >
+                        +
+                      </button>
+                    ) : index === formData.bid_inscope.length - 2 ? (
+                      <button
+                        className="ti-btn ti-btn-danger-full mt-4"
+                        onClick={() => handleRemoveBid(index, "bid_inscope")}
+                      >
+                        Remove
+                      </button>
+                    ) : null}
+                  </div>
+                </div>
+              ))}
+
+              {formData.bid_outscope.map((bid, index) => (
+                <div className="row g-3 align-items-center" key={index}>
+                  <div className="col-md-5">
+                    <label
+                      htmlFor={`bid_outscope_${index}`}
+                      className="form-label"
+                    >
+                      Bid Outscope {index + 1}
+                    </label>
+                    <input
+                      type="text"
+                      id={`bid_outscope_${index}`}
+                      name="bidDetail"
+                      value={bid.bidDetail}
+                      onChange={(e) =>
+                        handleBidChange(e, index, "bid_outscope")
+                      }
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="col-md-2">
+                    {index === formData.bid_outscope.length - 1 ? (
+                      <button
+                        className="ti-btn ti-btn-success-full mt-4"
+                        onClick={() => handleAddBid("bid_outscope")}
+                      >
+                        +
+                      </button>
+                    ) : index > 0 ? (
+                      <button
+                        className="ti-btn ti-btn-danger-full mt-4"
+                        onClick={() => handleRemoveBid(index, "bid_outscope")}
+                      >
+                        Remove
+                      </button>
+                    ) : null}
+                  </div>
+                </div>
+              ))}
+
               <div className="mb-4">
                 <label htmlFor="bid_payment_terms" className="form-label">
                   Bid Payment Terms
@@ -392,11 +384,7 @@ const CreateUpdateBid = (props) => {
                 />
               </div>
             </div>
-            <button
-              className="ti-btn ti-btn-primary-full"
-              type="submit"
-              data-hs-overlay="#hs-full-screen-modal"
-            >
+            <button className="ti-btn ti-btn-primary-full" type="submit">
               {formType === "update" ? "Update Bid" : "Create Bid"}
             </button>
           </form>
