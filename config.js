f172bf0f97d22c1cdd7702d80696260038b7ef3d
Sahil <s1610.2003@gmail.com>
2024-05-16 22:45:55 +0530

    gantt charrt

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const network = {
-    serverUrl : 'http://localhost:3000/',
+    serverUrl : 'https://ynex.webwisesolution.me/',
     onlineUrl : 'https://backend-api-topaz.vercel.app/',
     token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjQsImlhdCI6MTcxNTg0OTc4M30.DpXYgKJBljMAuVq1GyKYgkE2prmwt2SEvneeWJdoZWw'
 };
